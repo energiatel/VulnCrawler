c710b4d3bfcaf608b8163e21aa74a63a1d725adc
energiatel <energiatel@gmail.com>
2019-08-16 09:39:22 +0200

    Update crawl.sh

--- crawl.sh
+++ crawl.sh
@@ -23,11 +23,6 @@ clean_links_file()
 			log "$url start with Javascript:" $DIR/log
 			continue
 		fi
-		if [[ $url == javascript* ]]
-		then
-			log "$url start with Javascript:" $DIR/log
-			continue
-		fi
 		
 		#Delete tel:
 		if [[ $url == tel* ]]
@@ -37,14 +32,64 @@ clean_links_file()
 		fi
 		
 		#Delete media files:
-		if [[ $url == *jpg ]] || [[ $url == *png ]]
+		if [[ $url == *jpg ]] || [[ $url == *png ]] || [[ $url == *pdf ]] || [[ $url == *zip ]]
 		then
 			log "$url start with tel:" $DIR/log
 			continue
 		fi
 		
+		#Avoid gov tld domain:
+		if [[ $url == *.gov* ]]
+		then
+			log "$url is gov:" $DIR/log
+			continue
+		fi
+		
+		#If url starts with [:
+		if [[ $url == \[* ]]
+		then
+			log "$url starts with [" $DIR/log
+			continue
+		fi
+		
 		#Delete social: flickr, twitter, youtube, facebook, google, mozilla, microsoft	and other unwanted string
-		if [[ $url == *google*  ]] || [[ $url == *flickr*  ]] || [[ $url == *twitter*  ]] || [[ $url == *facebook*  ]] || [[ $url == *mozilla*  ]] || [[ $url == *microsoft*  ]] || [[ $url == *jigsaws*  ]] || [[ $url == *trenitalia*  ]] || [[ $url == *treccani*  ]] || [[ $url == *iubenda*  ]] || [[ $url == *w3*  ]] || [[ $url == *amazon*  ]] || [[ $url == *linkedin*  ]] || [[ $url == *corriere*  ]] || [[ $url == *hwupgrade*  ]] || [[ $url == *wiki*  ]] || [[ $url == *clickbank*  ]] || [[ $url == *repubblica*  ]] || [[ $url == *ilsole24ore*  ]] || [[ $url == *localhost*  ]] || [[ $url == *instagram*  ]]
+		if [[ $url == *google*  ]] || \
+			[[ $url == *flickr*  ]]  || \
+			[[ $url == *youtube*  ]]  || \
+			[[ $url == *youtu\.be*  ]]  || \
+			[[ $url == *meetup*  ]]  || \
+			[[ $url == *itunes*  ]]  || \
+			[[ $url == *pinterest*  ]]  || \
+			[[ $url == *etsy*  ]]  || \
+			[[ $url == *placeit*  ]]  || \
+			[[ $url == whatsapp*  ]]  || \
+			[[ $url == *vimeo*  ]]  || \
+			[[ $url == *github*  ]]  || \
+			[[ $url == *dribble*  ]] || \
+			[[ $url == *twitter*  ]] || \
+			[[ $url == *facebook*  ]] || \
+			[[ $url == *mozilla*  ]] || \
+			[[ $url == *microsoft*  ]] || \
+			[[ $url == *jigsaws*  ]] || \
+			[[ $url == *envato*  ]] || \
+			[[ $url == *trenitalia*  ]] || \
+			[[ $url == *treccani*  ]] || \
+			[[ $url == *iubenda*  ]] || \
+			[[ $url == *w3*  ]] || \
+			[[ $url == *amazon*  ]] || \
+			[[ $url == *linkedin*  ]] || \
+			[[ $url == *corriere*  ]] || \
+			[[ $url == *hwupgrade*  ]] || \
+			[[ $url == *wiki*  ]] || \
+			[[ $url == *clickbank*  ]] || \
+			[[ $url == *repubblica*  ]] || \
+			[[ $url == *ilsole24ore*  ]] || \
+			[[ $url == *localhost*  ]] || \
+			[[ $url == *instagram*  ]] || \
+			[[ $url == *wordpress\.com*  ]] || \
+			[[ $url == *godaddy*  ]] || \
+			[[ $url == *porn*  ]] || \
+			[[ $url == *graphicriver*  ]]
 		then
 			log "$url contains social string" $DIR/log
 			continue
@@ -73,9 +118,8 @@ get_links()
 		FILE=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 		
 		log "--- Visito $1 ---" "$DIR/log"
-		wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" -T 10 --quiet "$1" -O "$DIR/$FILE.html"
+		wget --tries=5 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" -T 10 --quiet "$1" -O "$DIR/$FILE.html"
 		lynx -listonly -nonumbers -hiddenlinks=merge -dump "$DIR/$FILE.html" > "$DIR/links_$FILE.txt"
-		#cat "$DIR/$FILE.html" | grep -o -E '\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))' >> "$DIR/links_$FILE.txt"
 		
 		if [[ -s "$DIR/links_$FILE.txt" ]]
 		then
@@ -86,6 +130,12 @@ get_links()
 		rm -f "$DIR/links_$FILE.txt"
 }
 
+if [[ $1 == 'clean' ]]
+then
+	echo 'clean'
+	exit
+fi
+
 echo "$1" >> "$DIR/links.txt"
 
 while [ -s "$DIR/links.txt" ]
@@ -93,13 +143,9 @@ do
 	site=$(head -1 "$DIR/links.txt" | sed 's/&/\%26/')
 	tail -n +2 "$DIR/links.txt" > "$DIR/links.txt.tmp" && mv "$DIR/links.txt.tmp" "$DIR/links.txt"
 	is_visited=$(grep -w "^$site$" "$DIR/visited.txt" | wc -l)
-	printf "\n\r"
-	echo "$site is_visited=$is_visited"
-	if [[ $is_visited -gt 0 ]]
+	
+	if [[ $is_visited -eq 0 ]]
 	then
-		echo "DUPLICATE"
-		printf "\n\r"
-	else
 		echo "$site" >> "$DIR/visited.txt"
 		echo "DOING -> $site"
 		get_links "$site" &
